b51a0d2de36b4bfc9c6a4f3bd2fd1c9b26ec9ead
mehedi-hasan-shuvon <mehedi.hasan.shuvon@gmail.com>
2023-04-20 00:44:50 +0600

    added bootrap and style our site

--- includes/header.php
+++ includes/header.php
@@ -4,21 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>PHP Primer - <?php echo $title ?></title>
 </head>
 <body>
+    <div class="container">
     <!-- this is a comment   -->
     <h2>Follow each links for practice</h2>
     <br />
 
-    <ul>
-        <li><a  href="index.php">Home</a></li>
-        <li><a  href="ifStatements.php">IF ELSE</a></li>
-        <li><a  href="switchStatements.php">Switch Statements</a></li>
-        <li><a  href="forLoop.php">For Loop</a></li>
-        <li><a  href="whileLoop.php">While Loop</a></li>
-        <li><a  href="array.php">Array</a></li>
-        <li><a  href="stringManipulation.php">String Manipulation</a></li>
-        <li><a  href="dateAndTime.php">Date and Time Manipulation</a></li>
-        <li><a  href="functions.php">Functions</a></li>
+    <ul class="nav">
+        <li class="nav-item"><a class="nav-link active"  href="index.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link"  href="ifStatements.php">IF ELSE</a></li>
+        <li class="nav-item"><a  class="nav-link" href="switchStatements.php">Switch Statements</a></li>
+        <li class="nav-item"><a class="nav-link"  href="forLoop.php">For Loop</a></li>
+        <li class="nav-item"><a  class="nav-link" href="whileLoop.php">While Loop</a></li>
+        <li class="nav-item"><a  class="nav-link" href="array.php">Array</a></li>
+        <li class="nav-item"><a class="nav-link"  href="stringManipulation.php">String Manipulation</a></li>
+        <li class="nav-item"><a class="nav-link"  href="dateAndTime.php">Date and Time Manipulation</a></li>
+        <li class="nav-item"><a  class="nav-link" href="functions.php">Functions</a></li>
     </ul>
+
